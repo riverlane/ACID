304d4257c921fd7a62153915c7c6c8c723626131
DaoyiC <daoyi.chen@riverlane.com>
2026-08-13 15:03:37 +0100

    refactor: add TODO comment regarding potential boolean usage for homomorphism parameters

--- ACID/src/acid/codes/bb/builder_deg5.py
+++ ACID/src/acid/codes/bb/builder_deg5.py
@@ -61,6 +61,7 @@ def build_code_from_spec(
         use_fx = fx if use_fx is None else use_fx
         use_fy = fy if use_fy is None else use_fy
 
+    # TODO: could be a bool since just 1 or 0
     bb = BBMidCycle(ring, A, B, homomorphism_f_x=int(use_fx), homomorphism_f_y=int(use_fy))
     embedding = SquareGridEmbedding(ring=bb.ring, pitch=1.0)
 
